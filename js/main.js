77d99d24da882c409bd8c9901916a67c0d295a47
MichalRzavucki <michael.rzheutski@gmail.com>
2021-05-31 01:52:20 +0300

    Update 2021-05-30T22:52:14.163Z

--- js/main.js
+++ js/main.js
@@ -11,7 +11,13 @@ $(function() {
   $('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
-    asNavFor: '.header__slider'
+    asNavFor: '.header__slider',
+    responsive: [
+      {
+        breakpoint: 961,
+        settings: 'unslick'
+      },
+    ]
   });
 
   $('.surf-slider').slick({
@@ -19,7 +25,35 @@ $(function() {
     slidesToScroll: 1,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
-    asNavFor: '.slider-map'
+    asNavFor: '.slider-map',
+    responsive: [
+      {
+        breakpoint: 1210,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true
+        }
+      },
+      {
+        breakpoint: 426,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false
+        }
+      },
+    ]
   });
 
   $('.slider-map').slick({
@@ -27,7 +61,29 @@ $(function() {
     slidesToScroll: 1,
     arrows: false,
     asNavFor: '.surf-slider',
-    focusOnSelect: true
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1103,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          centerMode: true
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true
+        }
+      },
+    ]
   });
 
   $('.holder__slider, .shop__slider').slick({
@@ -81,6 +137,10 @@ $(function() {
 
   $('.surfboard-box__circle').on('click', function() {
     $(this).toggleClass('active')
+  });
+
+  $('.menu-btn').on('click', function() {
+    $('.menu').toggleClass('active');
   });
 
 });
